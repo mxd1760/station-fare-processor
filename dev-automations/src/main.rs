2f4fdff31b70da686e8e7b5f8a8a35c3a089e1d4
Marcus Doucette <Marz3587@gmail.com>
2026-08-30 21:42:21 -0400

    impliment new adr and add tokio tasks to server response handling

--- dev-automations/src/main.rs
+++ dev-automations/src/main.rs
@@ -2,21 +2,35 @@ use rumqttc::{MqttOptions, Event, Incoming, Client, QoS};
 use shared::TapEventData;
 use std::time::Duration;
 
+static ENT_ID:&str = "test-entrance-1";
+static EX_ID:&str = "test-exit-1";
+
 fn main(){
     let mut mqttoptions = MqttOptions::new("dev-automation-kiosk1", "localhost", 1883);
     mqttoptions.set_keep_alive(Duration::from_secs(5));
-    let (mut client, mut connection) = Client::new(mqttoptions, 10);
+    let (client, mut connection) = Client::new(mqttoptions, 10);
 
-    client.subscribe("kiosks/entrance-1/taps/ack",QoS::AtLeastOnce).unwrap();
+    client.subscribe(format!("ent-ex/{}/taps/ack",ENT_ID),QoS::AtLeastOnce).unwrap();
+    client.subscribe(format!("ent-ex/{}/taps/ack",EX_ID), QoS::AtLeastOnce).unwrap();
 
     let event = TapEventData{
-        kiosk_id: "entrance-1".to_string(),
+        kiosk_id: ENT_ID.to_owned(),
         card_id: "card-1".to_string(),
         kiosk_transaction_number: 1
     };
 
     let payload = serde_json::to_vec(&event).unwrap();
-    client.publish("kiosks/entrance-1/taps",QoS::AtLeastOnce, false, payload).unwrap();
+    client.publish(format!("ent-ex/{}/taps",ENT_ID),QoS::AtLeastOnce, false, payload).unwrap();
+
+    let event = TapEventData{
+        kiosk_id: EX_ID.to_owned(),
+        card_id: "card-1".to_string(),
+        kiosk_transaction_number: 1
+    };
+
+    let payload = serde_json::to_vec(&event).unwrap();
+    client.publish(format!("ent-ex/{}/taps",EX_ID),QoS::AtLeastOnce, false, payload).unwrap();
+
     for notification in connection.iter(){
         match notification {
             Ok(Event::Incoming(Incoming::Publish(p))) =>{
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,38 +1,58 @@
-use rumqttc::{Client, Event, Incoming, MqttOptions, QoS};
+use rumqttc::{AsyncClient, Event, Incoming, MqttOptions, Publish, QoS};
 use shared::TapEventData;
+use tokio::sync::Mutex;
 use std::collections::HashSet;
+use std::sync::Arc;
 use std::time::Duration;
 
-fn main() {
+struct ServerState{
+    client:AsyncClient,
+    seen_events:HashSet<String>
+}
+
+#[tokio::main]
+async fn main() {
     let mut mqttoptions = MqttOptions::new("fare-server", "localhost", 1883);
     mqttoptions.set_keep_alive(Duration::from_secs(5));
-    let (mut client, mut connection) = Client::new(mqttoptions, 10);
+    
+    let (client, mut event_loop) = AsyncClient::new(mqttoptions, 10);
+    client.subscribe("ent-ex/+/taps", QoS::AtLeastOnce).await.unwrap();
 
-    client.subscribe("kiosks/+/taps", QoS::AtLeastOnce).unwrap();
-    let mut seen_events: HashSet<String> = HashSet::new();
+    let seen_events: HashSet<String> = HashSet::new();
 
-    for notification in connection.iter() {
-        match notification {
+    let server_state = Arc::new(Mutex::new(ServerState{client,seen_events}));
+    loop{
+        match event_loop.poll().await {
             Ok(Event::Incoming(Incoming::Publish(p))) => {
-                let event: TapEventData = match serde_json::from_slice(&p.payload) {
-                    Ok(e) => e,
-                    Err(e) => { eprintln!("Bad payload: {e:?}"); continue; }
-                };
-
-                let event_id = format!(
-                    "{}-{}-{}", event.kiosk_id, event.card_id, event.kiosk_transaction_number
-                );
-                let is_new = seen_events.insert(event_id.clone());
-                println!("Received tap: {event:?} (new: {is_new})");
-
-                let ack_topic = format!("kiosks/{}/taps/ack", event.kiosk_id);
-                let ack_payload = format!(
-                    r#"{{"event_id":"{event_id}","status":"confirmed"}}"#
-                );
-                client.publish(ack_topic, QoS::AtLeastOnce, false, ack_payload).unwrap();
+                tokio::spawn(handle_publish(Arc::clone(&server_state),p));
             }
             Ok(_) => {}
             Err(e) => { eprintln!("MQTT error: {e:?}"); break; }
         }
     }
+
+}
+
+async fn handle_publish(state:Arc<Mutex<ServerState>>,p:Publish)->Result<String,String>{
+    let event: TapEventData = match serde_json::from_slice(&p.payload) {
+        Ok(e) => e,
+        Err(e) => { 
+            let s = format!("Bad payload: {e:?}");
+            eprintln!("{s:?}"); 
+            return Err(s); 
+        }
+    };
+    let event_id = format!(
+        "{}-{}-{}", event.kiosk_id, event.card_id, event.kiosk_transaction_number
+    );
+    let ack_topic = format!("ent-ex/{}/taps/ack", event.kiosk_id);
+    let ack_payload = format!(
+        r#"{{"event_id":"{event_id}","status":"confirmed"}}"#
+    );
+
+    let mut state = state.lock().await;
+    let is_new = state.seen_events.insert(event_id.clone());
+    println!("Received tap: {event:?} (new: {is_new})");
+    state.client.publish(ack_topic, QoS::AtLeastOnce, false, ack_payload).await.unwrap();
+    Ok(event_id)
 }
