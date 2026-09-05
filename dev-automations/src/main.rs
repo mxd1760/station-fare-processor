3394187e7fac56dabcb168c370a7bdac0f649200
Marcus Doucette <Marz3587@gmail.com>
2026-09-05 16:45:35 -0400

    convert to clap
    closes #10

--- dev-automations/src/main.rs
+++ dev-automations/src/main.rs
@@ -1,11 +1,75 @@
 use rumqttc::{MqttOptions, Event, Incoming, Client, QoS};
 use shared::TapEventData;
-use std::time::Duration;
+use std::{path::Path, time::Duration};
+use clap::{Command, arg, command};
+
 
 static ENT_ID:&str = "test-entrance-1";
 static EX_ID:&str = "test-exit-1";
 
+enum FareProcessingMode{
+    Distance,
+    Segment,
+    Zone,
+    Pair
+}
+
+impl FareProcessingMode{
+    fn from_str(mode:&str)->Self{
+        match mode{
+            "Segment"=>Self::Segment,
+            "Zone"=>Self::Zone,
+            "Pair"=>Self::Pair,
+            _=>Self::Distance
+        }
+    }
+}
+
 fn main(){
+    let matches = command!()
+    .propagate_version(true)
+    .subcommand_required(true)
+    .arg_required_else_help(true)
+    .subcommand(
+        Command::new("test-mqtt")
+        .about("starts mqtt client loop and publishes 2 commands")
+    ).subcommand(
+        Command::new("solve-fares")
+        .about("read in map data with given ruleset to generate fare file for use on server")
+        .arg(arg!([file]).required(true))
+        .arg(arg!([mode]))
+    ).get_matches();
+
+    match matches.subcommand(){
+        Some(("test-mqtt",_sub_matches))=>{
+            test_mqtt()
+        },
+        Some(("solve-fares",sub_matches))=>{
+            let path = Path::new(sub_matches.get_one::<String>("file").unwrap());
+            let mode = FareProcessingMode::from_str(match sub_matches.get_one::<String>("mode") {
+                Some(v)=>v,
+                _=>""
+            });
+            solve_fares(path,mode)
+        }
+        _ => todo!(),
+    }
+}
+
+fn solve_fares(file:&Path,mode:FareProcessingMode){
+    match mode{
+        FareProcessingMode::Distance=>{
+            println!("parsing based on distance rules");
+
+        },
+        _=>{
+            println!("oops, you provided an invalid fare processing mode");
+        }
+    }
+}
+
+
+fn test_mqtt(){
     let mut mqttoptions = MqttOptions::new("dev-automation-kiosk1", "localhost", 1883);
     mqttoptions.set_keep_alive(Duration::from_secs(5));
     let (client, mut connection) = Client::new(mqttoptions, 10);
@@ -46,5 +110,4 @@ fn main(){
             },
         }
     }
-
 }
